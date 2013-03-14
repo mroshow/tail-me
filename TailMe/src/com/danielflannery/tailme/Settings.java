c9008ac5ff08889089a5d92d90f832b036ecbb37
ragebflame <ragebflame@2010a43a-e116-ac12-7397-93343f1bd650>
2013-03-14 18:24:48 +0000

    - Some bug fixes - Added an instructions dialog directing to the desktop app - Code cleanup and comments added.

--- TailMe/src/com/danielflannery/tailme/Settings.java
+++ TailMe/src/com/danielflannery/tailme/Settings.java
@@ -1,3 +1,9 @@
+/* Author: Dan
+ * Summary: This activity is used to display the settings of the application. 
+ * Any changes made here will be saved to sharedPreferences. 
+ * Extending PreferenceActivity is just a convenience class that derives from 
+ * ListActivity and allows to bootstrap the preferences UI from an XML file.
+ */
 
 package com.danielflannery.tailme;
 
@@ -7,8 +13,9 @@ import android.preference.PreferenceActivity;
 public class Settings extends PreferenceActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {        
-        super.onCreate(savedInstanceState);        
-        addPreferencesFromResource(R.xml.preferences);        
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Inflate the preferences xml file
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
