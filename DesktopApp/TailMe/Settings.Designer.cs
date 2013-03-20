a0e425d98038259c33bf929a4aef7a853826b21e
ragebflame <ragebflame@2010a43a-e116-ac12-7397-93343f1bd650>
2013-03-20 17:50:47 +0000

    Initial Commit

---
+++ DesktopApp/TailMe/Settings.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace TailMe
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+            this.exit_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.location_textbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tolerance_textbox = new System.Windows.Forms.TextBox();
+            this.pr_description = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // exit_button
+            // 
+            this.exit_button.Location = new System.Drawing.Point(172, 209);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(97, 37);
+            this.exit_button.TabIndex = 0;
+            this.exit_button.Text = "Save and Exit";
+            this.exit_button.UseVisualStyleBackColor = true;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Location = new System.Drawing.Point(12, 209);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(101, 37);
+            this.cancel_button.TabIndex = 1;
+            this.cancel_button.Text = "Cancel";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.location_textbox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(257, 174);
+            this.panel1.TabIndex = 2;
+            // 
+            // location_textbox
+            // 
+            this.location_textbox.Location = new System.Drawing.Point(99, 141);
+            this.location_textbox.Name = "location_textbox";
+            this.location_textbox.Size = new System.Drawing.Size(138, 20);
+            this.location_textbox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Your location: ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tolerance_textbox);
+            this.groupBox1.Controls.Add(this.pr_description);
+            this.groupBox1.Location = new System.Drawing.Point(6, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Point reduction tolerance";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tolerance:";
+            // 
+            // tolerance_textbox
+            // 
+            this.tolerance_textbox.Location = new System.Drawing.Point(70, 74);
+            this.tolerance_textbox.Name = "tolerance_textbox";
+            this.tolerance_textbox.Size = new System.Drawing.Size(161, 20);
+            this.tolerance_textbox.TabIndex = 1;
+            // 
+            // pr_description
+            // 
+            this.pr_description.AutoSize = true;
+            this.pr_description.Location = new System.Drawing.Point(6, 16);
+            this.pr_description.Name = "pr_description";
+            this.pr_description.Size = new System.Drawing.Size(0, 13);
+            this.pr_description.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(177, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Enable point reduction algorithm";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Application Settings:";
+            // 
+            // Settings
+            // 
+            this.AcceptButton = this.exit_button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(281, 254);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.exit_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Settings";
+            this.Text = "TailMe | Settings";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tolerance_textbox;
+        private System.Windows.Forms.Label pr_description;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox location_textbox;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ DesktopApp/TailMe/Settings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TailMe
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+            checkButtonState();
+            pr_description.Text = "The tolerance value will vary the point reduction \nresults. A larger number will produce less points."+
+                " \n\n                                      Default value = 0.0001";
+            tolerance_textbox.Text = Properties.Settings.Default.tolerance.ToString();
+            location_textbox.Text = Properties.Settings.Default.location;
+            checkBox1.Checked = Properties.Settings.Default.enable_pr;
+        }
+
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.tolerance = Double.Parse(tolerance_textbox.Text);
+            Properties.Settings.Default.location = location_textbox.Text;
+            Properties.Settings.Default.enable_pr = checkBox1.Checked;
+            Properties.Settings.Default.Save();
+            this.Close();
+        }
+
+        private void checkButtonState()
+        {
+            if (checkBox1.Checked == true)
+            {
+                tolerance_textbox.Enabled = true;
+            }
+            if (checkBox1.Checked == false)
+            {
+                tolerance_textbox.Enabled = false;
+            }
+        }
+
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                tolerance_textbox.Enabled = true;
+            }
+            if (checkBox1.Checked == false)
+            {
+                tolerance_textbox.Enabled = false;
+            }
+        }
+
+    }
+}
